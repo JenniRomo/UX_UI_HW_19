d37fa9dd763bd4f34f2615478198ec4955042d06
JenniRomo <romojennifer01@gmail.com>
2023-05-21 16:42:04 -0400

    updated jquery syntax

--- js/index.js
+++ js/index.js
@@ -4,18 +4,18 @@ var btn1 = $("#btn1")[0];
 var btn2 = $("#btn2")[0];
 var slider = $("slider")[0];
  
-btn1.onclick = function openUX() {
+$("#btn1").on("click", function openUX() {
     $('#content1').css('transform', 'translateX(0)');
     $('#content2').css('transform', 'translateX(200%)');
     $('#btn1').css('color', "#F5EEE6")
     $('#btn2').css('color', "#5053A4")
     $("#slider").css('transform', 'translateX(0)')
-  }
+  });
 
-btn2.onclick = function openGRD() {
+$("#btn2").on("click", function openGRD() {
     $('#content1').css('transform', 'translateX(200%)');
     $('#content2').css('transform', 'translateX(0)');
     $('#btn2').css('color', "#F5EEE6")
     $('#btn1').css('color', "#5053A4")
     $("#slider").css('transform', 'translateX(105px)')
-  }
+  });
